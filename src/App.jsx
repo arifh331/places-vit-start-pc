123a500f4a88cb89f619ef984f3ac1d5f481d3f1
Arif Howlader <ah3288@nyu.edu>
2023-05-22 20:20:53 -0400

    index.css change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import data from './data.js'
 import Card from './Card.jsx'
 import Header from './Header.jsx'
-import 'index.css'
+import './index.css'
 
 export default function App(){
 
