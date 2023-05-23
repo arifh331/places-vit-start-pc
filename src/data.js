ee8480ceea50bcd662b90fd819969ba86509bc9c
Arif Howlader <ah3288@nyu.edu>
2023-05-23 16:18:31 -0400

    Changed dates in datajs

--- src/data.js
+++ src/data.js
@@ -37,8 +37,8 @@ export default [
         title: "Konoha",
         location: "Land of Fire",
         googleMapsUrl: "https://www.google.com/search?q=konoha&rlz=1C1VDKB_enUS1025US1027&ei=RyRMZJf2N4Cs5NoP2J-csAw&ved=0ahUKEwjXtemkrc3-AhUAFlkFHdgPB8YQ4dUDCBA&uact=5&oq=konoha&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzILCAAQigUQsQMQkQIyBwgAEIoFEEMyCggAEIoFELEDEEMyCggAEIoFELEDEEMyBwgAEIoFEEMyCAgAEIAEELEDMgsILhCABBCxAxDUAjIICAAQgAQQsQMyBQgAEIAEMgUIABCABDoKCAAQRxDWBBCwAzoKCAAQigUQsAMQQzoNCAAQ5AIQ1gQQsAMYAToPCC4QigUQyAMQsAMQQxgCOhIILhCKBRDUAhDIAxCwAxBDGAI6CAguEIoFEJECOggIABCKBRCRAjoLCC4QgAQQsQMQgwE6BQguEIAEOhAILhCKBRCxAxDHARDRAxAKOgsILhCKBRCxAxCDAToOCC4QgAQQsQMQxwEQ0QM6CwguEIAEEMcBENEDOhYILhCKBRCRAhDcBBCXBRDeBBDfBBgDOgoILhCKBRCxAxBDOgcILhCKBRBDOhAILhCKBRCxAxDHARDRAxBDOggILhCABBCxAzoLCAAQgAQQsQMQgwE6FgguEIoFEJECENwEEJcFEN4EEOAEGAM6CwguENQCELEDEIAEOggILhCABBDUAkoECEEYAFCJD1i8FmCKGGgGcAF4AIABjwGIAb0FkgEDMS41mAEAoAEByAETwAEB2gEGCAEQARgJ2gEGCAIQARgI2gEGCAMQARgU&sclient=gws-wiz-serp",
-        startDate: "23 November, 2014",
-        endDate: "23 November, 2014",
+        startDate: "5 November, 2003",
+        endDate: "23 January, 2012",
         description: "Konoha, the Village Hidden in the Leaves, is a serene oasis embraced by lush greenery. Its vibrant streets are alive with the energy of bustling villagers, their footsteps echoing amidst traditional architecture framed by nature's beauty. Within this hidden sanctuary, a sense of harmony thrives as the villagers go about their daily lives, embodying the spirit of this captivating ninja village.",
         imageUrl: "./jcolmall.jpeg"
         
@@ -48,8 +48,8 @@ export default [
         title: "Atlantis",
         location: "Unknown",
         googleMapsUrl: "https://www.google.com/search?q=atlantis&rlz=1C1VDKB_enUS1025US1027&ei=5SZMZP_TBZ-u5NoP6ZWqEA&ved=0ahUKEwi_2vTjr83-AhUfF1kFHemKCgIQ4dUDCBA&uact=5&oq=atlantis&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzILCAAQigUQsQMQkQIyEQguEK8BEMcBENQCELEDEIAEMggILhCABBDUAjIICC4QsQMQgAQyCAgAEIAEELEDMgsILhCABBDHARCvATIFCAAQgAQyCwguEIAEEMcBEK8BMg4ILhCABBCxAxDHARCvATIICC4QgAQQ1AI6CggAEEcQ1gQQsAM6CggAEIoFELADEEM6DQgAEOQCENYEELADGAE6EgguEIoFENQCEMgDELADEEMYAjoHCAAQigUQQzoRCC4QgAQQsQMQgwEQxwEQ0QM6CwgAEIAEELEDEIMBOg4ILhCABBCxAxDHARDRAzoLCC4QigUQsQMQgwE6CwguEIAEELEDEIMBOg0ILhCKBRCxAxDUAhBDOhsILhCKBRCxAxDUAhBDENwEEJcFEN4EEOAEGAM6EQguEIoFEMcBEK8BENQCEJECOgsILhCKBRCxAxCRAjoLCC4QgAQQsQMQ1AI6CwguENQCELEDEIAEOgoIABCABBCxAxAKOggILhCABBCxAzofCC4QigUQxwEQrwEQ1AIQkQIQ3AQQlwUQ3gQQ4AQYAzoOCC4QrwEQxwEQsQMQgAQ6GQguENQCELEDEIAEENwEEJcFEN4EEOAEGAM6DgguEIoFELEDENQCEJECOg0ILhCABBCxAxCDARAKOgoILhCABBCxAxAKOg4ILhDUAhCxAxCKBRCRAjoHCAAQgAQQCjoTCC4QgAQQsQMQxwEQrwEQ1AIQCjoLCAAQigUQsQMQgwE6HAguEIoFELEDENQCEJECENwEEJcFEN4EEOAEGAM6EAguEMcBELEDENEDEIAEEAo6DQgAEIAEELEDEIMBEAo6EQguEIoFELEDEIMBEMcBENEDOhMILhCABBCxAxCDARDHARDRAxAKOhMILhCvARDHARDUAhCxAxCABBAKOgoILhCABBDUAhAKOgoILhCxAxCABBAKOg0ILhCABBDHARCvARAKOhAILhCABBCxAxDHARCvARAKOgsILhCKBRDUAhCRAjoKCC4QigUQsQMQQzoRCC4QgAQQsQMQxwEQrwEQ1AI6DgguEMcBELEDENEDEIAESgQIQRgAUKAGWN8mYN8oaAhwAXgAgAHTAYgB6A6SAQYxLjEyLjGYAQCgAQHIARHAAQHaAQYIARABGAnaAQYIAhABGAjaAQYIAxABGBQ&sclient=gws-wiz-serp",
-        startDate: "23 November, 2014",
-        endDate: "23 November, 2014",
+        startDate: "23 April, 1996",
+        endDate: "23 March, 1997",
         description: "Hidden beneath the ocean's embrace, Atlantis beckons with its enigmatic beauty. Crystal spires rise, shimmering with an otherworldly luminescence. Within, a realm of vibrant gardens and whispered legends awaits, captivating the imagination with the allure of an ancient and extraordinary civilization.",
         imageUrl: "./jcolmall.jpeg"
         
@@ -59,8 +59,8 @@ export default [
         title: "Jedi Temple",
         location: "Coruscant",
         googleMapsUrl: "https://www.google.com/search?q=jedi+temple&rlz=1C1VDKB_enUS1025US1027&ei=zSdMZJ3uMumg5NoPjcOhgAs&ved=0ahUKEwjdiPLSsM3-AhVpEFkFHY1hCLAQ4dUDCBA&uact=5&oq=jedi+temple&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzILCAAQgAQQsQMQgwEyCwgAEIAEELEDEIMBMgsIABCABBCxAxCDATIFCC4QgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQguEIAEMgUIABCABDIFCAAQgAQ6CwguEIoFELEDEIMBOg4ILhCABBCxAxDHARDRAzoLCAAQigUQsQMQgwE6GQguEIoFELEDEIMBENwEEJcFEN4EEN8EGAE6EAguEIoFELEDEMcBENEDEEM6BwgAEIoFEEM6BwguEIoFEEM6CgguEIoFENQCEEM6CwguEIAEELEDEIMBOh4ILhCKBRCxAxDHARDRAxBDENwEEJcFEN4EEOAEGAE6CwguEIMBELEDEIAEOggIABCKBRCRAjoICC4QigUQkQI6GQguEIMBELEDEIAEENwEEJcFEN4EEOAEGAE6CwguEK8BEMcBEIAESgQIQRgAUABYuw1gzw9oAHABeACAAaEBiAG1CJIBAzYuNZgBAKABAcABAdoBBggBEAEYFA&sclient=gws-wiz-serp",
-        startDate: "23 November, 2014",
-        endDate: "23 November, 2014",
+        startDate: "23 January, 2000",
+        endDate: "1 May , 2005",
         description: "On Coruscant, the Jedi Temple stands tall amidst the urban sprawl, an elegant sanctuary of serenity. Its graceful spires pierce the sky, casting a tranquil aura over the bustling cityscape below.",
         imageUrl: "./jcolmall.jpeg"
         
